af7501926b3afb25910f0bf8f17ba44a72a8fbce
zyxkad <zyxkad@gmail.com>
2023-10-18 07:38:35 -0600

    implement Tag

--- tag.go
+++ tag.go
@@ -1,16 +1,39 @@
 
 package liter
 
+import (
+	"io"
+)
+
 type Tag struct {
-	//
+	Name    Identifier
+	Entries []VarInt
 }
 
 var _ Packet = (*Tag)(nil)
 
-func (c Tag)Encode(b *PacketBuilder){
-	//
+func (t Tag)Encode(b *PacketBuilder){
+	b.String(t.Name)
+	b.VarInt((VarInt)(len(t.Entries)))
+	for _, v := range t.Entries {
+		b.VarInt(v)
+	}
 }
 
-func (c *Tag)DecodeFrom(r *PacketReader)(err error){
+func (t *Tag)DecodeFrom(r *PacketReader)(err error){
+	var ok bool
+	var n VarInt
+	if t.Name, ok = r.String(); !ok {
+		return io.EOF
+	}
+	if n, ok = r.VarInt(); !ok {
+		return io.EOF
+	}
+	t.Entries = make([]VarInt, n)
+	for i, _ := range t.Entries {
+		if t.Entries[i], ok = r.VarInt(); !ok {
+			return io.EOF
+		}
+	}
 	return
 }
